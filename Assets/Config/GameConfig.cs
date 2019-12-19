105e4526def9391f02dd90af5c6843ac8c94d93e
yu <yuri.yudin@kosmos.games>
2019-12-19 17:20:39 +0300

    draft player conrtroller

---
+++ Assets/Config/GameConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameConfig", menuName = "GameConfig")]
+public class GameConfig : ScriptableObject
+{
+    public int defaultSheepsCount = 12;
+    public PlatformConfig Platform;
+    public SheepConfig sheep;
+}
+
+[Serializable]
+public class SheepConfig
+{
+    public float speed = 1;
+}
+
+[Serializable]
+public class PlatformConfig
+{
+    public int defaultMaxSheepCapacity = 12;
+    public int decreaseCapacityFactor = 1;
+    
+    public float appearPeriod = 30f;
+    public float highlightTime = 5f;
+}
---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,41 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class PlayerInputController : MonoBehaviour
+{
+    private Camera camera;
+    public float speed;
+    public float turnDuration = 0.2f;
+    public Transform plane;
+    private float movingGap = 0.5f;
+
+    private void Awake()
+    {
+        camera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 targetPoint = GetPlaneIntersection(plane, camera);
+            transform.LookAt(targetPoint);
+            var delta = (targetPoint - transform.position);
+            if (delta.magnitude <= movingGap)
+            {
+                return;
+            }
+            var moveVector = delta.normalized * speed;
+            transform.position += moveVector;
+        }
+    }
+    
+    Vector3 GetPlaneIntersection(Transform plane, Camera camera)
+    {
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        float delta = ray.origin.y - plane.position.y;
+        Vector3 dirNorm = ray.direction / ray.direction.y;
+        Vector3 intersection = ray.origin - dirNorm * delta;
+        return intersection;
+    }
+}
