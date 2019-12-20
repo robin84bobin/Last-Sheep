28c0c8eb06638d253109b5d32db74fa775e7f98c
yu <yuri.yudin@kosmos.games>
2019-12-21 00:43:58 +0300

    bot move controller draft

---
+++ Assets/Scripts/BotMoveController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class BotMoveController : MonoBehaviour
+{
+    public float speed;
+    public float turnDuration = 0.2f;
+    public Transform plane;
+    public float movingGap = 2f;
+    private CharacterController characterController;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        var moveVector = Vector3.zero;
+        //TODO some logic...
+        characterController.SimpleMove(moveVector);
+    }
+}
